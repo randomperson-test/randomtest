25eef333620da30844e145fcfce78bb419fc3e8d
randomperson-test <jackfromvietnamlol@gmail.com>
2026-07-05 10:22:56 +0700

    Add files via upload

---
+++ main21.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+CSV_FILE = "D:\\py3082\\data2.csv"
+
+
+df = pd.read_csv(CSV_FILE)
+
+st.dataframe(df)
+
+if st.button("Random"):
+
+    randomized_df = df.copy()
+
+    numeric_cols = randomized_df.select_dtypes(
+        include=["number"]
+    ).columns
+
+    for col in numeric_cols:
+        randomized_df[col] = np.random.randint(
+            1, 11,
+            size=len(randomized_df)
+        )
+
+    randomized_df.to_csv(CSV_FILE, index=False)
